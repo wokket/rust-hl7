8f2ac070d2a43f0ae87080eaa71ab0e2202a6a9e
wokket <wokket@gmail.com>
2019-01-18 12:28:10 +1000

    feat: first shot at field value selection...

--- src/main.rs
+++ src/main.rs
@@ -10,20 +10,31 @@ fn main() {
 
     let sw = Stopwatch::start_new();
 
-    for _ in 0..1000000 {
-        let message = pipe_parser::message_parser::MessageParser::parse_message(input);
-    }
-    let duration = sw.elapsed_ms();
-    println!("1000,000 messges parsed in {}ms", duration);
-    println!(
-        "This means we parsed {} msgs/sec",
-        (1000000 / (duration / 1000))
-    );
+    // for _ in 0..1000000 {
+    //     let _ = pipe_parser::message_parser::MessageParser::parse_message(input);
+    // }
+    // let duration = sw.elapsed_ms();
+    // println!("1000,000 messges parsed in {}ms", duration);
+    // println!(
+    //     "This means we parsed {} msgs/sec",
+    //     (1000000 / (duration / 1000))
+    // );
+
+    let mut msg = pipe_parser::message_parser::MessageParser::parse_message(get_simple_message());
+    let msa = msg.get_segments("MSA");
+
+    println!("{:?}", msa);
+
+    msg = pipe_parser::message_parser::MessageParser::parse_message(get_sample_message());
+    let obr = msg.get_segments("OBR");
+
+    println!("{:?}", obr[0].fields[16].get_all_as_string());
 }
 
 fn get_sample_message() -> &'static str {
-    r#" MSH|^~\&|GHH LAB|ELAB-3|GHH OE|BLDG4|200202150930||ORU^R01|CNTRL-3456|P|2.4
- PID|||555-44-4444||EVERYWOMAN^EVE^E^^^^L|JONES|19620320|F|||153 FERNWOOD DR.^^STATESVILLE^OH^35292||(206)3345232|(206)752-121||||AC555444444||67-A4335^OH^20030520\r
- OBR|1|845439^GHH OE|1045813^GHH LAB|15545^GLUCOSE|||200202150730|||||||||555-55-5555^PRIMARY^PATRICIA P^^^^MD^^|||||||||F||||||444-44-4444^HIPPOCRATES^HOWARD H^^^^MD\r
- OBX|1|SN|1554-5^GLUCOSE^POST 12H CFST:MCNC:PT:SER/PLAS:QN||^182|mg/dl|70_105|H|||F"#
+    "MSH|^~\\&|GHH LAB|ELAB-3|GHH OE|BLDG4|200202150930||ORU^R01|CNTRL-3456|P|2.4\rPID|||555-44-4444||EVERYWOMAN^EVE^E^^^^L|JONES|19620320|F|||153 FERNWOOD DR.^^STATESVILLE^OH^35292||(206)3345232|(206)752-121||||AC555444444||67-A4335^OH^20030520\rOBR|1|845439^GHH OE|1045813^GHH LAB|15545^GLUCOSE|||200202150730|||||||||555-55-5555^PRIMARY^PATRICIA P^^^^MD^^|||||||||F||||||444-44-4444^HIPPOCRATES^HOWARD H^^^^MD\rOBX|1|SN|1554-5^GLUCOSE^POST 12H CFST:MCNC:PT:SER/PLAS:QN||^182|mg/dl|70_105|H|||F\r"
+}
+
+fn get_simple_message() -> &'static str {
+    "MSH|^~\\&|CATH|StJohn|AcmeHIS|StJohn|20061019172719||ACK^O01|MSGID12349876|P|2.3\rMSA|AA|MSGID12349876"
 }
--- src/pipe_parser/mod.rs
+++ src/pipe_parser/mod.rs
@@ -1,8 +1,11 @@
+extern crate itertools;
 extern crate test;
 
 mod field_parser;
 pub mod message_parser;
 mod segment_parser;
+
+use itertools::Itertools;
 
 /// A repeat of a field is a set of 0 or more sub component values.
 /// Currently all values are stored as their original string representations.  Methods to convert
@@ -31,4 +34,147 @@ pub struct Segment<'a> {
 #[derive(Debug, PartialEq)]
 pub struct Message<'a> {
     pub segments: Vec<Segment<'a>>,
+}
+
+impl<'a> Repeat<'a> {
+    pub fn get_as_string(&self) -> String {
+        if self.sub_components.len() == 0 {
+            return "".to_string();
+        } else {
+            return self.sub_components.join("^");
+        }
+    }
+}
+
+impl<'a> Field<'a> {
+    /// Returns a single String built from all the repeats.segment_parser
+    /// This value includes HL7 delimiter values between repeats, components and sub components.segment_parser
+    /// A copy  of the oringla data is made here (rather than a &str) as the value is expected to be returned to external callers who
+    /// shouldn't have to keep the entire source message alive
+    pub fn get_all_as_string(&self) -> String {
+        if self.repeats.len() == 0 {
+            return "".to_string();
+        }
+
+        self.repeats.iter().map(|r| r.get_as_string()).join("~")
+    }
+}
+
+impl<'a> Message<'a> {
+    pub fn get_segments(&self, segment_type: &str) -> Vec<&Segment<'a>> {
+        self.segments
+            .iter()
+            .filter(|segment| segment.fields[0].get_all_as_string() == segment_type)
+            .collect()
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn repeat_get_all_as_string_single_simple_value() {
+        let r = Repeat {
+            sub_components: vec!["Simple Repeat"],
+        };
+
+        let actual = r.get_as_string();
+        assert_eq!(actual, "Simple Repeat");
+    }
+
+    #[test]
+    fn repeat_get_all_as_string_multi_components() {
+        let r = Repeat {
+            sub_components: vec!["Multiple", "Components"],
+        };
+
+        let actual = r.get_as_string();
+        assert_eq!(actual, "Multiple^Components");
+    }
+
+    #[test]
+    fn field_get_all_as_string_single_simple_value() {
+        let f = Field {
+            repeats: vec![Repeat {
+                sub_components: vec!["Simple Repeat"],
+            }],
+        };
+
+        let actual = f.get_all_as_string();
+        assert_eq!(actual, "Simple Repeat");
+    }
+
+    #[test]
+    fn field_get_all_as_string_multiple_repeats() {
+        let f = Field {
+            repeats: vec![
+                Repeat {
+                    sub_components: vec!["Repeat 1"],
+                },
+                Repeat {
+                    sub_components: vec!["Repeat 2"],
+                },
+            ],
+        };
+
+        let actual = f.get_all_as_string();
+        assert_eq!(actual, "Repeat 1~Repeat 2");
+    }
+
+    #[test]
+    fn test_segment_lookup() {
+        let msg = message_parser::MessageParser::parse_message("MSH|fields\rOBR|segment\r"); //note the trailing \r
+                                                                                             /*let expected = Message {
+                                                                                                 segments: vec![
+                                                                                                     Segment {
+                                                                                                         fields: vec![
+                                                                                                             Field {
+                                                                                                                 repeats: vec![Repeat {
+                                                                                                                     sub_components: vec!["test"],
+                                                                                                                 }],
+                                                                                                             },
+                                                                                                             Field {
+                                                                                                                 repeats: vec![Repeat {
+                                                                                                                     sub_components: vec!["fields"],
+                                                                                                                 }],
+                                                                                                             },
+                                                                                                         ],
+                                                                                                     },
+                                                                                                     Segment {
+                                                                                                         fields: vec![
+                                                                                                             Field {
+                                                                                                                 repeats: vec![Repeat {
+                                                                                                                     sub_components: vec!["another"],
+                                                                                                                 }],
+                                                                                                             },
+                                                                                                             Field {
+                                                                                                                 repeats: vec![Repeat {
+                                                                                                                     sub_components: vec!["segment"],
+                                                                                                                 }],
+                                                                                                             },
+                                                                                                         ],
+                                                                                                     },
+                                                                                                 ],
+                                                                                             };*/
+
+        let expected = Segment {
+            fields: vec![
+                Field {
+                    repeats: vec![Repeat {
+                        sub_components: vec!["OBR"],
+                    }],
+                },
+                Field {
+                    repeats: vec![Repeat {
+                        sub_components: vec!["segment"],
+                    }],
+                },
+            ],
+        };
+
+        let result = msg.get_segments("OBR");
+        assert!(result.len() == 1);
+        assert_eq!(expected, *result[0]);
+    }
 }

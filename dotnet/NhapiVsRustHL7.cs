5831fdff093a1814d682debbce6fc9014dac908c
wokket <wokket@gmail.com>
2019-01-31 11:47:41 +1000

    feat: Added HL7-DotNetCore as a comparison.
    It's being actively maintained, is .Net Standard, and is roughly twice as fast as NHapi despite them publishing a debug build   >:(

--- dotnet/NhapiVsRustHL7.cs
+++ dotnet/NhapiVsRustHL7.cs
@@ -1,5 +1,4 @@
-﻿using System.Linq;
-using BenchmarkDotNet.Attributes;
+﻿using BenchmarkDotNet.Attributes;
 using NHapi.Base.Parser;
 using NHapi.Base.Util;
 using NHapi.Model.V24.Message;
@@ -11,59 +10,71 @@ namespace ConsoleApp1
 
     public class NhapiVsRustHL7
     {
-        public const string ACK_TEXT = "MSH|^~\\&|SENDING_APPLICATION|SENDING_FACILITY|RECEIVING_APPLICATION|RECEIVING_FACILITY|20110614075841||ACK|1407511|P|2.3||||||\r\n" +
+        public const string ACK_TEXT = "MSH|^~\\&|SENDING_APPLICATION|SENDING_FACILITY|RECEIVING_APPLICATION|RECEIVING_FACILITY|20110614075841||ACK|1407511|P|2.3||||||\r" +
                                             "MSA|AA|1407511|Success||";
 
-        public const string ORU_TEXT = @"MSH|^~\&|GHH LAB|ELAB-3|GHH OE|BLDG4|200202150930||ORU^R01|CNTRL-3456|P|2.4
- PID|||555-44-4444||EVERYWOMAN^EVE^E^^^^L|JONES|19620320|F|||153 FERNWOOD DR.^^STATESVILLE^OH^35292||(206)3345232|(206)752-121||||AC555444444||67-A4335^OH^20030520\r
- OBR|1|845439^GHH OE|1045813^GHH LAB|15545^GLUCOSE|||200202150730|||||||||555-55-5555^PRIMARY^PATRICIA P^^^^MD^^|||||||||F||||||444-44-4444^HIPPOCRATES^HOWARD H^^^^MD\r
- OBX|1|SN|1554-5^GLUCOSE^POST 12H CFST:MCNC:PT:SER/PLAS:QN||^182|mg/dl|70_105|H|||F";
-
-        // instantiate a PipeParser, which handles the "traditional or default encoding"
-        private PipeParser _parser;
-        private ORU_R01 _oru;
-
-        [GlobalSetup]
-        public void Setup()
+        public const string ORU_TEXT = 
+ "MSH|^~\\&|GHH LAB|ELAB-3|GHH OE|BLDG4|200202150930||ORU^R01|CNTRL-3456|P|2.4\r" +
+ "PID|||555-44-4444||EVERYWOMAN^EVE^E^^^^L|JONES|19620320|F|||153 FERNWOOD DR.^^STATESVILLE^OH^35292||(206)3345232|(206)752-121||||AC555444444||67-A4335^OH^20030520\r" +
+ "OBR|1|845439^GHH OE|1045813^GHH LAB|15545^GLUCOSE|||200202150730|||||||||555-55-5555^PRIMARY^PATRICIA P^^^^MD^^|||||||||F||||||444-44-4444^HIPPOCRATES^HOWARD H^^^^MD\r" +
+ "OBX|1|SN|1554-5^GLUCOSE^POST 12H CFST:MCNC:PT:SER/PLAS:QN||^182|mg/dl|70_105|H|||F";
+       
+        [Benchmark]
+        public void NHAPI_Build_Ack()
         {
-            _parser = new PipeParser();
-            _oru = _parser.Parse(ORU_TEXT) as ORU_R01;
+            var parser = new PipeParser();
+            var hl7Message = parser.Parse(ACK_TEXT);
         }
 
         [Benchmark]
-        public void NHAPI_Ack()
+        public void Hl7DotNetCore_Build_Ack()
         {
-            var hl7Message = _parser.Parse(ACK_TEXT);
+            var hl7Message = new HL7.Dotnetcore.Message(ACK_TEXT);
+            hl7Message.ParseMessage();
         }
 
         [Benchmark]
-        public void NHAPI_Oru_Parse()
+        public void Rust_Build_Ack()
         {
-            var hl7Message = _parser.Parse(ORU_TEXT);
+            var hl7Message = Native.BuildMessage(ACK_TEXT);
         }
 
+        [Benchmark]
+        public void NHAPI_Build_Oru()
+        {
+            var parser = new PipeParser();
+            var hl7Message = parser.Parse(ORU_TEXT);
+        }
 
+        [Benchmark]
+        public void Hl7DotNetCore_Build_Oru()
+        {
+            var hl7Message = new HL7.Dotnetcore.Message(ORU_TEXT);
+            hl7Message.ParseMessage();
+        }
+
+        [Benchmark]
+        public void Rust_Build_Oru()
+        {
+            using (var hl7Message = Native.BuildMessage(ORU_TEXT)) { }
+        }
 
         [Benchmark]
         public void NHAPI_Parse_and_retrieve_field()
         {
-
-            var hl7Message = _parser.Parse(ORU_TEXT) as NHapi.Model.V24.Message.ORU_R01;
+            var parser = new PipeParser();
+            var hl7Message = parser.Parse(ORU_TEXT) as NHapi.Model.V24.Message.ORU_R01;
             var t = new Terser(hl7Message);
             var fieldValueAsString = t.Get("/.OBR-7"); //get a rando field from the middle of the thing
 
         }
 
         [Benchmark]
-        public void Rust_Ack()
+        public void Hl7DotNetCore_Parse_and_retrieve_field()
         {
-            var hl7Message = Native.BuildMessage(ACK_TEXT);
-        }
-
-        [Benchmark]
-        public void Rust_Oru_Parse()
-        {
-            var hl7Message = Native.BuildMessage(ORU_TEXT);
+            var hl7Message = new HL7.Dotnetcore.Message(ORU_TEXT);
+            hl7Message.ParseMessage();
+            var fieldValueAsString = hl7Message.GetValue("OBR.7"); //get a rando field from the middle of the thing
         }
 
         [Benchmark]
--- dotnet/Program.cs
+++ dotnet/Program.cs
@@ -1,7 +1,10 @@
-﻿using BenchmarkDotNet.Running;
+﻿using BenchmarkDotNet.Configs;
+using BenchmarkDotNet.Running;
+using BenchmarkDotNet.Validators;
 using NHapi.Base.Parser;
 using NHapi.Base.Util;
 using System;
+using System.Linq;
 
 namespace ConsoleApp1
 {
@@ -9,7 +12,8 @@ namespace ConsoleApp1
     {
        private static void Main(string[] args)
         {
-            using (var mh = Native.BuildMessage(ORU_TEXT))
+            //RUST
+            using (var mh = Native.BuildMessage(NhapiVsRustHL7.ORU_TEXT))
             { //pointer to Message
 
                 using (var fieldValue = Native.GetField(mh.DangerousGetHandle(), "OBR", 7))
@@ -18,23 +22,32 @@ namespace ConsoleApp1
                 } //dispose of string handle, freeing up string memeory on the rust side.
             }
 
-            //var parser = new PipeParser();
-            //var hl7Message = parser.Parse(ORU_TEXT) as NHapi.Model.V24.Message.ORU_R01;
-            //var t = new Terser(hl7Message);
-            //var field = t.Get("/.OBR-7"); //get a rando field from the middle of the thing
+            //HL7-DotNetCore
+            var hl7Message = new HL7.Dotnetcore.Message(NhapiVsRustHL7.ORU_TEXT);
+            hl7Message.ParseMessage();
+            var v = hl7Message.GetValue("OBR.7"); //get a rando field from the middle of the thing
+
+            //NHAPI
+            var parser = new PipeParser();
+            var hl7Message2 = parser.Parse(NhapiVsRustHL7.ORU_TEXT) as NHapi.Model.V24.Message.ORU_R01;
+            var t = new Terser(hl7Message2);
+            var field = t.Get("/.OBR-7"); //get a rando field from the middle of the thing
 
             //Console.Read();
-            var summary = BenchmarkRunner.Run<NhapiVsRustHL7>();
+            var summary = BenchmarkRunner.Run<NhapiVsRustHL7>( new AllowNonOptimized()); //HL7-DotNet has published a debug build :(
             
         }
 
-       
+        public class AllowNonOptimized : ManualConfig
+        {
+            public AllowNonOptimized()
+            {
+                Add(JitOptimizationsValidator.DontFailOnError); // ALLOW NON-OPTIMIZED DLLS
 
-
-
-        const string ORU_TEXT = @"MSH|^~\&|GHH LAB|ELAB-3|GHH OE|BLDG4|200202150930||ORU^R01|CNTRL-3456|P|2.4
-PID|||555-44-4444||EVERYWOMAN^EVE^E^^^^L|JONES|19620320|F|||153 FERNWOOD DR.^^STATESVILLE^OH^35292||(206)3345232|(206)752-121||||AC555444444||67-A4335^OH^20030520
-OBR|1|845439^GHH OE|1045813^GHH LAB|15545^GLUCOSE|||200202150730|||||||||555-55-5555^PRIMARY^PATRICIA P^^^^MD^^|||||||||F||||||444-44-4444^HIPPOCRATES^HOWARD H^^^^MD
-OBX|1|SN|1554-5^GLUCOSE^POST 12H CFST:MCNC:PT:SER/PLAS:QN||^182|mg/dl|70_105|H|||F";
+                Add(DefaultConfig.Instance.GetLoggers().ToArray()); // manual config has no loggers by default
+                Add(DefaultConfig.Instance.GetExporters().ToArray()); // manual config has no exporters by default
+                Add(DefaultConfig.Instance.GetColumnProviders().ToArray()); // manual config has no columns by default
+            }
+        }
     }
 }

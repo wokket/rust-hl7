ee2c33420271fdd542926cc0ee5ae3585c177fe5
wokket <wokket@gmail.com>
2019-02-03 07:13:33 +1000

    feat: Strip some symbols to reduce library size on *nix.

--- build_and_run.sh
+++ build_and_run.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
 cargo b --release 
+
+#strip out debugging constants etc from the library. reduces *.nix library from 4Mb back to (a windows equalling) 1.4Mb
+strip ./target/release/librusthl7.so
+
 cp ./target/release/librusthl7.so ./dotnet 
 dotnet run -c release --project ./dotnet/ConsoleApp1.csproj

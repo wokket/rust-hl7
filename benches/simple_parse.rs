86c086bb5505fe6118dacbb3eb9b52c3dcf7a9e1
Tim Thompson <wokket@gmail.com>
2021-07-23 20:34:15 +1000

    feat: Moved to Criterion so Benchmarks run on stable

--- benches/simple_parse.rs
+++ benches/simple_parse.rs
@@ -1,16 +1,15 @@
-#![feature(test)]
-extern crate test;
+use criterion::{criterion_group, criterion_main, Criterion};
+use rusthl7::message::*;
 
-#[cfg(test)]
-mod benches {
-    use rusthl7::message::*;
+
     fn get_sample_message() -> String {
         "MSH|^~\\&|GHH LAB|ELAB-3|GHH OE|BLDG4|200202150930||ORU^R01|CNTRL-3456|P|2.4\rPID|||555-44-4444||EVERYWOMAN^EVE^E^^^^L|JONES|19620320|F|||153 FERNWOOD DR.^^STATESVILLE^OH^35292||(206)3345232|(206)752-121||||AC555444444||67-A4335^OH^20030520\rOBR|1|845439^GHH OE|1045813^GHH LAB|15545^GLUCOSE|||200202150730|||||||||555-55-5555^PRIMARY^PATRICIA P^^^^MD^^|||||||||F||||||444-44-4444^HIPPOCRATES^HOWARD H^^^^MD\rOBX|1|SN|1554-5^GLUCOSE^POST 12H CFST:MCNC:PT:SER/PLAS:QN||^182|mg/dl|70_105|H|||F".to_string()
     }
 
-    #[bench]
-    fn message_parse(b: &mut test::Bencher) {
+    fn message_parse(c: &mut Criterion) {
         let msg = &get_sample_message();
-        b.iter(|| Message::from_str(msg));
+        c.bench_function("oru parse", |b| b.iter(|| Message::from_str(msg)));
     }
-}
+    
+    criterion_group!(benches, message_parse);
+    criterion_main!(benches);

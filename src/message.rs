7bafdd8a53141ffb4b4c927b702e7976d3222963
RageLtMan <rageltman [at] sempervictus>
2021-07-28 23:34:15 -0400

    Add test for segs_to_str_vecs() and rstfmt

--- src/message.rs
+++ src/message.rs
@@ -32,7 +32,9 @@ impl<'a> Message<'a> {
 
     /// Extracts header element to an owned object for external use
     pub fn msh(&self) -> Result<msh::MshSegment, Hl7ParseError> {
-        let segment = self.segments.iter()
+        let segment = self
+            .segments
+            .iter()
             .find_map(|s| match s {
                 segments::Segment::MSH(x) => Some(x.clone().to_owned()),
                 _ => None,
@@ -43,32 +45,48 @@ impl<'a> Message<'a> {
 
     /// Extracts all generic elements to owned objects for external use
     pub fn generic_segments(&self) -> Result<Vec<&generic::GenericSegment>, Hl7ParseError> {
-        let generics: Vec<&generic::GenericSegment> = self.segments.iter()
+        let generics: Vec<&generic::GenericSegment> = self
+            .segments
+            .iter()
             .filter_map(|s| match s {
                 segments::Segment::Generic(x) => Some(x),
                 _ => None,
-            }).map(|g| g.clone().to_owned()).collect();
+            })
+            .map(|g| g.clone().to_owned())
+            .collect();
         Ok(generics)
     }
 
     /// Extracts generic elements to owned objects for external use by matching first field to name
-    pub fn generic_segments_by_name(&self, name: &str) -> Result<Vec<&generic::GenericSegment>, Hl7ParseError> {
-        let found: Vec<&generic::GenericSegment> = self.segments.iter()
+    pub fn generic_segments_by_name(
+        &self,
+        name: &str,
+    ) -> Result<Vec<&generic::GenericSegment>, Hl7ParseError> {
+        let found: Vec<&generic::GenericSegment> = self
+            .segments
+            .iter()
             .filter_map(|s| match s {
                 segments::Segment::Generic(x) => {
-                    if x.fields.first().unwrap().value() == name { Some(x) } else { None }
-                },
+                    if x.fields.first().unwrap().value() == name {
+                        Some(x)
+                    } else {
+                        None
+                    }
+                }
                 _ => None,
-            }).map(|g| g.clone().to_owned()).collect();
+            })
+            .map(|g| g.clone().to_owned())
+            .collect();
         Ok(found)
     }
 
     /// Present input vectors of &generics to vectors of &str
-    pub fn segs_to_str_vecs(segments: Vec<&generic::GenericSegment<'a>>) -> Result<Vec<Vec<&'a str>>, Hl7ParseError> {
-        let vecs = segments.iter()
-            .map(|s| s.fields.iter()
-                .map(|f| f.value())
-                .collect())
+    pub fn segs_to_str_vecs(
+        segments: Vec<&generic::GenericSegment<'a>>,
+    ) -> Result<Vec<Vec<&'a str>>, Hl7ParseError> {
+        let vecs = segments
+            .iter()
+            .map(|s| s.fields.iter().map(|f| f.value()).collect())
             .collect();
         Ok(vecs)
     }
@@ -111,6 +129,18 @@ mod tests {
         let msg = Message::from_str(hl7)?;
 
         assert_eq!(msg.msh().unwrap().msh_1_field_separator, '|');
+        Ok(())
+    }
+    #[test]
+    fn ensure_segments_convert_to_vectors() -> Result<(), Hl7ParseError> {
+        let hl7 = "MSH|^~\\&|GHH LAB|ELAB-3|GHH OE|BLDG4|200202150930||ORU^R01|CNTRL-3456|P|2.4\rOBR|segment";
+        let msg = Message::from_str(hl7)?;
+        let segs = msg.generic_segments_by_name("OBR")?;
+        let sval = segs.first().unwrap().fields.first().unwrap().value();
+        let vecs = Message::segs_to_str_vecs(segs).unwrap();
+        let vval = vecs.first().unwrap().first().unwrap();
+
+        assert_eq!(vval, &sval);
         Ok(())
     }
 }

6ddf460c4a29c1d365322e0f218fdf509921c177
Tim Thompson <sthompsont@worldvision.com.au>
2021-08-08 14:30:48 +1000

    feat: removed string indexer... not really applicable for querying by path

--- src/fields/mod.rs
+++ src/fields/mod.rs
@@ -1,9 +1,12 @@
+use std::fmt::Display;
+use std::ops::Index;
 use super::separators::Separators;
 use super::*;
+
+
 /// Represents a single field inside the HL7.  Note that fields can include repeats, components and sub-components.
 /// See [the spec](http://www.hl7.eu/HL7v2x/v251/std251/ch02.html#Heading13) for more info
 #[derive(Debug, PartialEq)]
-
 pub struct Field<'a> {
     pub source: &'a str,
     pub delims: Separators,
@@ -29,7 +32,7 @@ impl<'a> Field<'a> {
         Ok(field)
     }
 
-    /// Used to hide the removal of NoneError for #2...  If passed `Some()` value it retursn a field with that value.  If passed `None() it returns an `Err(Hl7ParseError::MissingRequiredValue{})`
+    /// Used to hide the removal of NoneError for #2...  If passed `Some()` value it returns a field with that value.  If passed `None() it returns an `Err(Hl7ParseError::MissingRequiredValue{})`
     pub fn parse_mandatory(
         input: Option<&'a str>,
         delims: &Separators,
@@ -62,51 +65,10 @@ impl<'a> Field<'a> {
     pub fn as_str(&self) -> &'a str {
         self.source
     }
-}
 
-use std::fmt::Display;
-impl<'a> Display for Field<'a> {
-    /// Required for to_string() and other formatter consumers
-    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-        write!(f, "{}", self.source)
-    }
-}
-
-use std::ops::Index;
-impl<'a> Clone for Field<'a> {
-    /// Creates a new Message object using a clone of the original's source
-    fn clone(&self) -> Self {
-        Field::parse(self.source, &self.delims.clone()).unwrap()
-    }
-}
-
-/// Access string reference of a Field component by numeric index
-impl<'a> Index<usize> for Field<'a> {
-    type Output = &'a str;
-    fn index(&self, idx: usize) -> &Self::Output {
-        if idx > self.components.len() - 1 {
-            return &"";
-        }
-        &self.components[idx]
-    }
-}
-
-/// Access string reference of a Field subcomponent by numeric index
-impl<'a> Index<(usize, usize)> for Field<'a> {
-    type Output = &'a str;
-    fn index(&self, idx: (usize, usize)) -> &Self::Output {
-        if idx.0 > self.components.len() - 1 || idx.1 > self.subcomponents[idx.0].len() - 1 {
-            return &"";
-        }
-        &self.subcomponents[idx.0][idx.1]
-    }
-}
-
-/// Access string reference of a Field component by String index
-/// Adjust the index by one as medical people do not count from zero
-impl<'a> Index<String> for Field<'a> {
-    type Output = &'a str;
-    fn index(&self, sidx: String) -> &Self::Output {
+    /// Access string reference of a Field component by String index
+    /// Adjust the index by one as medical people do not count from zero
+    pub fn query(&self, sidx: &str) -> &'a str {
         let parts = sidx.split('.').collect::<Vec<&str>>();
 
         if parts.len() == 1 {
@@ -134,17 +96,52 @@ impl<'a> Index<String> for Field<'a> {
 
             &self[(idx0 - 1, idx1 - 1)]
         } else {
-            &""
+            ""
         }
+    }
+
+    /// Access Segment, Field, or sub-field string references by string index
+    pub fn query_by_string(&self, idx: String) -> &'a str {
+        &self.query(idx.as_str())
+    }
+
+}
+
+
+impl<'a> Display for Field<'a> {
+    /// Required for to_string() and other formatter consumers
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        write!(f, "{}", self.source)
     }
 }
 
-impl<'a> Index<&str> for Field<'a> {
-    type Output = &'a str;
 
-    /// Access Segment, Field, or sub-field string references by string index
-    fn index(&self, idx: &str) -> &Self::Output {
-        &self[String::from(idx)]
+impl<'a> Clone for Field<'a> {
+    /// Creates a new Message object using a clone of the original's source
+    fn clone(&self) -> Self {
+        Field::parse(self.source, &self.delims.clone()).unwrap()
+    }
+}
+
+/// Access string reference of a Field component by numeric index
+impl<'a> Index<usize> for Field<'a> {
+    type Output = &'a str;
+    fn index(&self, idx: usize) -> &Self::Output {
+        if idx > self.components.len() - 1 {
+            return &"";
+        }
+        &self.components[idx]
+    }
+}
+
+/// Access string reference of a Field subcomponent by numeric index
+impl<'a> Index<(usize, usize)> for Field<'a> {
+    type Output = &'a str;
+    fn index(&self, idx: (usize, usize)) -> &Self::Output {
+        if idx.0 > self.components.len() - 1 || idx.1 > self.subcomponents[idx.0].len() - 1 {
+            return &"";
+        }
+        &self.subcomponents[idx.0][idx.1]
     }
 }
 
@@ -246,10 +243,9 @@ mod tests {
         let d = Separators::default();
         let f = Field::parse_mandatory(Some("xxx^yyy&zzz"), &d).unwrap();
         let idx0 = String::from("R2");
-        let idx1 = String::from("R2.C2");
-        let oob = String::from("R2.C3");
-        assert_eq!(f[idx0.clone()], "yyy&zzz");
-        assert_eq!(f[idx1], "zzz");
-        assert_eq!(f[oob], "");
+        let oob = "R2.C3";
+        assert_eq!(f.query_by_string(idx0.clone()), "yyy&zzz");
+        assert_eq!(f.query("R2.C2"), "zzz");
+        assert_eq!(f.query(oob), "");
     }
 }

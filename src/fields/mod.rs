e2b86293407812cdefa142aaadf0c47675e0a5d2
Tim Thompson <github@wokket.com>
2021-08-11 07:34:49 +1000

    rebase onto master @ 0.4.0

--- src/fields/mod.rs
+++ src/fields/mod.rs
@@ -1,9 +1,12 @@
+use std::fmt::Display;
+use std::ops::Index;
 use super::separators::Separators;
 use super::*;
+
+
 /// Represents a single field inside the HL7.  Note that fields can include repeats, components and sub-components.
 /// See [the spec](http://www.hl7.eu/HL7v2x/v251/std251/ch02.html#Heading13) for more info
 #[derive(Debug, PartialEq)]
-
 pub struct Field<'a> {
     pub source: &'a str,
     pub delims: Separators,
@@ -29,7 +32,7 @@ impl<'a> Field<'a> {
         Ok(field)
     }
 
-    /// Used to hide the removal of NoneError for #2...  If passed `Some()` value it retursn a field with that value.  If passed `None() it returns an `Err(Hl7ParseError::MissingRequiredValue{})`
+    /// Used to hide the removal of NoneError for #2...  If passed `Some()` value it returns a field with that value.  If passed `None() it returns an `Err(Hl7ParseError::MissingRequiredValue{})`
     pub fn parse_mandatory(
         input: Option<&'a str>,
         delims: &Separators,
@@ -62,9 +65,49 @@ impl<'a> Field<'a> {
     pub fn as_str(&self) -> &'a str {
         self.source
     }
+
+    /// Access string reference of a Field component by String index
+    /// Adjust the index by one as medical people do not count from zero
+    pub fn query(&self, sidx: &str) -> &'a str {
+        let parts = sidx.split('.').collect::<Vec<&str>>();
+
+        if parts.len() == 1 {
+            let stringnums = parts[0]
+                .chars()
+                .filter(|c| c.is_digit(10))
+                .collect::<String>();
+            let idx: usize = stringnums.parse().unwrap();
+
+            &self[idx - 1]
+        } else if parts.len() == 2 {
+            let stringnums = parts[0]
+                .chars()
+                .filter(|c| c.is_digit(10))
+                .collect::<String>();
+
+            let idx0: usize = stringnums.parse().unwrap();
+
+            let stringnums = parts[1]
+                .chars()
+                .filter(|c| c.is_digit(10))
+                .collect::<String>();
+
+            let idx1: usize = stringnums.parse().unwrap();
+
+            &self[(idx0 - 1, idx1 - 1)]
+        } else {
+            ""
+        }
+    }
+
+    /// Access Segment, Field, or sub-field string references by string index
+    pub fn query_by_string(&self, idx: String) -> &'a str {
+        &self.query(idx.as_str())
+    }
+
 }
 
-use std::fmt::Display;
+
 impl<'a> Display for Field<'a> {
     /// Required for to_string() and other formatter consumers
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
@@ -72,7 +115,7 @@ impl<'a> Display for Field<'a> {
     }
 }
 
-use std::ops::Index;
+
 impl<'a> Clone for Field<'a> {
     /// Creates a new Message object using a clone of the original's source
     fn clone(&self) -> Self {
@@ -250,10 +293,9 @@ mod tests {
         let d = Separators::default();
         let f = Field::parse_mandatory(Some("xxx^yyy&zzz"), &d).unwrap();
         let idx0 = String::from("R2");
-        let idx1 = String::from("R2.C2");
-        let oob = String::from("R2.C3");
-        assert_eq!(f[idx0.clone()], "yyy&zzz");
-        assert_eq!(f[idx1], "zzz");
-        assert_eq!(f[oob], "");
+        let oob = "R2.C3";
+        assert_eq!(f.query_by_string(idx0.clone()), "yyy&zzz");
+        assert_eq!(f.query("R2.C2"), "zzz");
+        assert_eq!(f.query(oob), "");
     }
 }

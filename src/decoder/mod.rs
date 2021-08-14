6696b55e9143197a7f421849d25e725dccd01f53
Tim Thompson <github@wokket.com>
2021-08-15 00:16:05 +1000

    feat: Added support for ignoring custom (\Zdd\) escape sequences

--- src/decoder/mod.rs
+++ src/decoder/mod.rs
@@ -11,24 +11,22 @@ pub struct EscapeSequence {
     repeat_buf: [u8; 1],
     component_buf: [u8; 1],
     subcomponent_buf: [u8; 1],
-
-    escape_regex : Regex
+    escape_regex: Regex,
 }
 
 impl<'a> EscapeSequence {
     pub fn new(delims: Separators) -> EscapeSequence {
-        
-
         let regex = if delims.escape_char == '\\' {
             Regex::new(r#"\\"#) // needs special handling because backslashes have meaning in regexes, and need to be escaped
         } else {
             Regex::new(String::from(delims.escape_char).as_str()) //everything else just works (I hope!)
-        }.unwrap();
-        
+        }
+        .unwrap();
+
         let mut return_val = EscapeSequence {
             delims,
             escape_buf: [0; 1], // TODO: Does the spec allow multi-byte delim chars??
-            field_buf: [0; 1], 
+            field_buf: [0; 1],
             repeat_buf: [0; 1],
             component_buf: [0; 1],
             subcomponent_buf: [0; 1],
@@ -46,10 +44,20 @@ impl<'a> EscapeSequence {
         return_val
     }
 
-
     /// This module contains the decoding functionality to parse escape sequences from the source string back to their original chars.
     ///
     /// For more info see [here](https://www.lyniate.com/knowledge-hub/hl7-escape-sequences/) or [here](https://confluence.hl7australia.com/display/OOADRM20181/Appendix+1+Parsing+HL7v2#Appendix1ParsingHL7v2-Dealingwithreservedcharactersanddelimiters)
+    ///
+    /// ## Example:
+    /// ```
+    /// # use rusthl7::decoder::EscapeSequence;
+    /// # use rusthl7::separators::Separators;
+    /// let delims = Separators::default();
+    /// let decoder = EscapeSequence::new(delims);
+    /// let hl7_field_value = r#"Obstetrician \T\ Gynaecologist"#;
+    /// let decoded = decoder.decode(hl7_field_value);
+    /// assert_eq!(decoded, r#"Obstetrician & Gynaecologist"#);
+    /// ```
     ///
     /// ## Details
     ///
@@ -59,10 +67,10 @@ impl<'a> EscapeSequence {
     /// - `\Z...\` - Custom application escape sequences, these are custom (as are most `Z` items in HL7) and will not be replaced
     ///
     /// Also, not all of the sequences that _should_ be replaced are currently being handled, specifically:
-    /// - `\E\`,`\F\`, '\R\`, `\S\`, `\T\` are all handledm and replaced with the Escape, Field, Repeat, Component and Sub-Componnet delimiter chars respectively
+    /// - `\E\`,`\F\`, '\R\`, `\S\`, `\T\` are all handled, and replaced with the Escape, Field, Repeat, Component and Sub-Component delimiter chars respectively
     /// - `\Cxxyy\`, '\Mxxyyzz\, '\Xdd..\` _should_ be handled, but aren't currently.  There's [some suggestion](https://confluence.hl7australia.com/display/OOADRM20181/Appendix+1+Parsing+HL7v2#Appendix1ParsingHL7v2-Unicodecharacters) that these are discouraged in lieu of html-escaped values
-    /// 
-    /// If there's _no possibility_ of escape sequences (because there's no escape characters, typically backspaces) in the value, this function short circuits as early as possible and returns the original string slice 
+    ///
+    /// If there's _no possibility_ of escape sequences (because there's no escape characters, typically backspaces) in the value, this function short circuits as early as possible and returns the original string slice
     /// for optimum performance.
     pub fn decode<S>(&self, input: S) -> Cow<'a, str>
     where
@@ -74,80 +82,97 @@ impl<'a> EscapeSequence {
         let input = input.into();
         let first = self.escape_regex.find(&input); //using `regex.find` here is about twice as fast for the 'no sequences' benchmark as using &str.find()...
 
-        if let Some(first) = first {
-            let first = first.start();
+        match first {
+            Some(first) => {
+                let first = first.start();
 
-            // We know there's a backslash, so we need to process stuff
+                // We know there's a backslash, so we need to process stuff
 
-            // we're going to be replacing (mainly) 3 char escape sequences (eg `\F\`) with a single char sequence (eg `|`) so the initial length of the input should be sufficient
-            let mut output: Vec<u8> = Vec::with_capacity(input.len());
-            output.extend_from_slice(input[0..first].as_bytes()); // this doesn't include the escape char we found
+                // we're going to be replacing (mainly) 3 char escape sequences (eg `\F\`) with a single char sequence (eg `|`) so the initial length of the input should be sufficient
+                let mut output: Vec<u8> = Vec::with_capacity(input.len());
+                output.extend_from_slice(input[0..first].as_bytes()); // this doesn't include the escape char we found
 
-            // index in input that we're up to
-            let mut i = first;
+                // index in input that we're up to
+                let mut i = first;
 
-            debug!("Found first escape char at {}", first);
+                debug!("Found first escape char at {}", first);
 
-            while i < input.len() {
-                let start_of_sequence = input[i..].find(self.delims.escape_char);
-                if start_of_sequence.is_none() {
-                    // there's nothing left to process, no more backslashes in the rest of the buffer
+                while i < input.len() {
+                    let start_of_sequence = input[i..].find(self.delims.escape_char);
+                    if start_of_sequence.is_none() {
+                        // there's nothing left to process, no more backslashes in the rest of the buffer
 
-                    trace!("No more sequence starts in input, completing...");
-                    output.extend_from_slice(input[i..].as_bytes()); // add the rest of the input
-                    break; // break out of while loop
-                }
-
-                let start_index = start_of_sequence.unwrap() + i; // index is offset into input by i chars as that's what's we subsliced above
-                trace!("Found the next escape char at {}", start_index);
-
-                let end_of_sequence = input[start_index + 1..].find(self.delims.escape_char);
-
-                if end_of_sequence.is_none() {
-                    // there's nothing left to process, the backslash we are curently looking at is NOT an escape sequence
-                    trace!("No more sequence ends in input, completing...");
-                    output.extend_from_slice(input[start_index..].as_bytes()); // add the rest of the input (including the escape char that brought us here) in one go
-                    break; // break out of while loop
-                }
-
-                // else we have found another escape char, get the slice in between
-                let end_index = end_of_sequence.unwrap() + start_index + 1; // the end is the number of chars after the start_index, not from the start of input
-                trace!("Found end of sequence at {}", end_index);
-
-                let sequence = &input[start_index + 1..end_index];
-                trace!("Found escape sequence: '{}'", sequence);
-
-                // we have a possible window of data between i and start_index that we've just read through as text, but isn't yet in output... append it now
-                output.extend_from_slice(input[i..start_index].as_bytes());
-
-                match sequence {
-                    "E" => output.extend_from_slice(&self.escape_buf),
-                    "F" => output.extend_from_slice(&self.field_buf),
-                    "R" => output.extend_from_slice(&self.repeat_buf),
-                    "S" => output.extend_from_slice(&self.component_buf),
-                    "T" => output.extend_from_slice(&self.subcomponent_buf),
-
-                    // Highlighted/Normal text sequences need to remain for consuming libraries to act on as they see fit
-                    "H" => output.extend_from_slice(r#"\H\"#.as_bytes()),
-                    "N" => output.extend_from_slice(r#"\N\"#.as_bytes()),
-                    // TODO: Add more sequences here
-                    _ => {
-                        // not a known sequence, must just be two backslashes randomly in a string
-                        trace!("Unknown sequence, extending output...");
-                        output.extend_from_slice(input[start_index - 1..end_index].as_bytes());
-                        // include both the initial escape char, and also the final one.
+                        trace!("No more sequence starts in input, completing...");
+                        output.extend_from_slice(input[i..].as_bytes()); // add the rest of the input
+                        break; // break out of while loop
                     }
-                }
 
-                i = end_index; // move through buffer, we we've covered everything up to this point now
+                    let start_index = start_of_sequence.unwrap() + i; // index is offset into input by i chars as that's what's we subsliced above
+                    trace!("Found the next escape char at {}", start_index);
 
-                i += 1;
-            } // while more chars in input to loop through
+                    let end_of_sequence = input[start_index + 1..].find(self.delims.escape_char);
 
-            Cow::Owned(String::from_utf8(output).unwrap())
-        } else {
-            // no escape char in the string at all, just return what we have
-            input
+                    if end_of_sequence.is_none() {
+                        // there's nothing left to process, the backslash we are curently looking at is NOT an escape sequence
+                        trace!("No more sequence ends in input, completing...");
+                        output.extend_from_slice(input[start_index..].as_bytes()); // add the rest of the input (including the escape char that brought us here) in one go
+                        break; // break out of while loop
+                    }
+
+                    // else we have found another escape char, get the slice in between
+                    let end_index = end_of_sequence.unwrap() + start_index + 1; // the end is the number of chars after the start_index, not from the start of input
+                    trace!("Found end of sequence at {}", end_index);
+
+                    let sequence = &input[start_index + 1..end_index];
+                    trace!("Found escape sequence: '{}'", sequence);
+
+                    // we have a possible window of data between i and start_index that we've just read through as text, but isn't yet in output... append it now
+                    output.extend_from_slice(input[i..start_index].as_bytes());
+
+                    match sequence {
+                        "E" => output.extend_from_slice(&self.escape_buf),
+                        "F" => output.extend_from_slice(&self.field_buf),
+                        "R" => output.extend_from_slice(&self.repeat_buf),
+                        "S" => output.extend_from_slice(&self.component_buf),
+                        "T" => output.extend_from_slice(&self.subcomponent_buf),
+
+                        // Highlighted/Normal text sequences need to remain for consuming libraries to act on as they see fit
+                        "H" | "N" => {
+                            output.extend_from_slice(&self.escape_buf);
+                            output.extend_from_slice(sequence.as_bytes());
+                            output.extend_from_slice(&self.escape_buf);
+                        }
+
+                        _ => {
+                            if sequence.starts_with('Z') {
+                                println!("Into custom escape sequence, ignoring...");
+                                output.extend_from_slice(&self.escape_buf);
+                                output.extend_from_slice(sequence.as_bytes());
+                                output.extend_from_slice(&self.escape_buf);
+
+                            // TODO: Add more sequences
+                            } else {
+                                // not a known sequence, must just be two backslashes randomly in a string
+                                trace!("Unknown sequence, extending output...");
+                                output.extend_from_slice(
+                                    input[start_index - 1..end_index].as_bytes(),
+                                );
+                                // include both the initial escape char, and also the final one.
+                            }
+                        }
+                    }
+
+                    i = end_index; // move through buffer, we we've covered everything up to this point now
+
+                    i += 1;
+                } // while more chars in input to loop through
+
+                Cow::Owned(String::from_utf8(output).unwrap())
+            }
+            None => {
+                // no escape char in the string at all, just return what we have
+                input
+            }
         }
     }
 }
@@ -259,6 +284,16 @@ mod tests {
         let escaper = EscapeSequence::new(delims);
 
         let input = r#"Don't escape this \H\highlighted text\N\ please"#;
+        let output = escaper.decode(input);
+        assert_eq!(output, input);
+    }
+
+    #[test]
+    fn ensure_decode_ignores_custom_sequence() {
+        let delims = Separators::default();
+        let escaper = EscapeSequence::new(delims);
+
+        let input = r#"Don't escape this custom sequence \Z1234\ please"#;
         let output = escaper.decode(input);
         assert_eq!(output, input);
     }

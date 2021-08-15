966e6bbd3269696ab01c81918ba49d04171a362d
Tim Thompson <github@wokket.com>
2021-08-15 10:41:51 +1000

    docs

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,21 @@
+/*!
+
+# RustHl7 - A HL7 V2 message parser
+
+This crate is attempting to provide a fully spec-compliant HL7 V2 message parser.  Note that _interpreting_ the parsed message elements into a strongly
+typed segment/message format is specifically **out of scope** as there's simply too many variants over too many versions for me to go there (maybe 
+someone else could code-gen a crate using this this crate to provide the source information?).  
+
+
+
+## Crate Features
+The default use of this crate should be as close to spec-compliant as we can manage.  If there's features you absolutely are sure you don't need (because, say, you control both ends of the wire)
+you can run with `default-features = false` and then opt back in to the features you _do_ need.
+
+- `escape-sequence`: This feature includes the decoding of [`EscapeSequence`] in the values returned from the `query` functions
+
+*/
+
 pub mod escape_sequence;
 pub mod fields;
 pub mod message;
